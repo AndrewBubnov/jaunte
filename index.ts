607389363f60449ed2c7c07cba7d77c49eed98e4
Andrii Bubnov <andrii.bubnov@cyrebro.io>
2023-04-23 19:28:02 +0200

    Create useComputedStore hook and overload the create function proper inferring types

--- index.ts
+++ index.ts
@@ -1,20 +1,21 @@
 import { useSyncExternalStore } from 'react';
 import {
-    Bound,
-    ComputedStoreCreator, CreateSimple,
+    ComputedStoreCreator,
+    CreateSimple,
     CreateWithComputed,
     FunctionalParam,
-    ObjectParam,
+    ObjectParam, Selector,
     SetStateAction,
     Store,
     StoreCreator,
     StoreCreatorItem,
-    SubscribeCallback, UseStore
+    SubscribeCallback,
+    UseStore
 } from "./types";
 
 const merge = (...args: object[]) => Object.assign({}, ...args);
 
-const useStore = <T, S extends T = T>(store: Store<T>, selector?: (state: T) => S) => {
+const useStore = <T, S extends T>(store: Store<T>, selector?: Selector<T, S>) => {
     const { getState, subscribe, persistKey } = store;
 
     if (persistKey) localStorage.setItem(persistKey, JSON.stringify(store.getState()));
@@ -22,12 +23,10 @@ const useStore = <T, S extends T = T>(store: Store<T>, selector?: (state: T) => 
     return useSyncExternalStore(subscribe, getSnapshot);
 };
 
-const useComputedStore = <T, S extends T = T>(bound: Bound<T>, selector?: (state: Partial<T>) => S) => {
-    const [store, computed] = bound;
-    const { getState } = store;
-
-    const computedState = computed(getState());
-    return selector ? selector(computedState) : computedState;
+const useComputedStore = <U, S extends U>(store: Store<U>, selector?: Selector<U, S>) => {
+    const { getState, subscribe } = store;
+    const getSnapshot = selector ? () => selector(getState()) : getState;
+    return useSyncExternalStore(subscribe, getSnapshot);
 };
 
 const createStore = <T>(storeCreatorArg: StoreCreator<T>): Store<T> => {
@@ -58,26 +57,73 @@ const createStore = <T>(storeCreatorArg: StoreCreator<T>): Store<T> => {
     };
 };
 
+const createStoreWithComputed = <T, U>(storeCreatorArg: StoreCreator<T>, computed: ComputedStoreCreator<T, U>) => {
+    const [storeCreator, persistKey, persisted] = Array.isArray(storeCreatorArg) ? storeCreatorArg : [storeCreatorArg];
+
+    let store = {} as T;
+    let computedStore = {} as U;
+
+    const subscribers = new Set<SubscribeCallback<T>>();
+    const computedSubscribers = new Set<SubscribeCallback<U>>();
+
+    const setter = (setStateAction: SetStateAction<T>) => {
+        const isFunction = typeof setStateAction === 'function';
+        const updated = isFunction ? (setStateAction as FunctionalParam<T>)(store) : (setStateAction as ObjectParam<T>);
+        store = merge(store as object, updated);
+        computedStore = computed(store);
+        subscribers.forEach(callback => callback(store));
+        computedSubscribers.forEach(callback => callback(computedStore));
+    };
+
+    store = storeCreator(setter);
+    computedStore = computed(store);
+
+    if (persistKey && persisted) store = merge(store as object, persisted);
+
+    const mainStoreApi = {
+        getState: () => store,
+        subscribe: (callback: SubscribeCallback<T>) => {
+            subscribers.add(callback);
+            return () => subscribers.delete(callback);
+        },
+        persistKey,
+    };
+    const computedStoreApi = {
+        getState: () => computedStore,
+        subscribe: (callback: SubscribeCallback<U>) => {
+            computedSubscribers.add(callback);
+            return () => computedSubscribers.delete(callback);
+        },
+    };
+    return [mainStoreApi, computedStoreApi] as [Store<T>, Store<U>];
+};
+
 const createSimple = (<T, S extends T>(storeCreator: StoreCreator<T, S>) => {
     const store = createStore(storeCreator);
-    const hook = (store: Store<T>, selector?: (state: T) => S) => useStore(store, selector);
+    const hook = (store: Store<T>, selector?: Selector<T, S>) => useStore(store, selector);
     return hook.bind(null, store);
 }) as CreateSimple;
 
-const createWithComputed = (<T, S extends T>(storeCreator: StoreCreator<T, S>, computed: ComputedStoreCreator<T>) => {
-    const store = createStore(storeCreator);
-    const hook = (store: Store<T>, selector?: (state: T) => S) => useStore(store, selector);
-    const computedHook = (bound: Bound<T>, selector?: (state: Partial<T>) => S) => useComputedStore(bound, selector);
-    return [hook.bind(null, store), computedHook.bind(null, [store, computed])];
+const createWithComputed = (<T, U, S extends T, V extends U>(
+    storeCreator: StoreCreator<T, S>,
+    computed: ComputedStoreCreator<T, U>
+) => {
+    const [mainStoreApi, computedStoreApi] = createStoreWithComputed(storeCreator, computed);
+    const hook = (store: Store<T>, selector?: Selector<T, S>) => useStore(store, selector);
+    const computedHook = (store: Store<U>, selector?: Selector<U, V>) => useComputedStore(store, selector);
+    return [hook.bind(null, mainStoreApi), computedHook.bind(null, computedStoreApi)];
 }) as CreateWithComputed;
 
-export function create<T, S extends T = T>(storeCreator: StoreCreator<T, S>): UseStore<Store<T>>;
-export function create<T, S extends T = T>(
+export function create<T, S extends T>(storeCreator: StoreCreator<T, S>): UseStore<Store<T>>;
+export function create<T, U = never, S extends T = T>(
     storeCreator: StoreCreator<T, S>,
-    computed: ComputedStoreCreator<T>
-): [UseStore<Store<T>>, UseStore<Store<T>>];
+    computed: ComputedStoreCreator<T, U>
+): [UseStore<Store<T>>, UseStore<Store<U>>];
 
-export function create<T, S extends T>(storeCreator: StoreCreator<T, S>, computed?: ComputedStoreCreator<T>) {
+export function create<T, U = never, S extends T = T>(
+    storeCreator: StoreCreator<T, S>,
+    computed?: ComputedStoreCreator<T, U>
+) {
     if (computed) return createWithComputed(storeCreator, computed);
     return createSimple(storeCreator);
 }
--- types.ts
+++ types.ts
@@ -1,22 +1,23 @@
-type Selector<T, S> = (state: Inferred<T>) => S;
 export type ObjectParam<S> = Partial<S>;
 export type FunctionalParam<S> = (arg: S) => Partial<S>;
 export type SetStateAction<S> = ObjectParam<S> | FunctionalParam<S>;
 export type StoreCreatorItem<T, U> = (set: (action: SetStateAction<T>) => void) => U;
 export type StoreCreator<T, U = T> = StoreCreatorItem<T, U> | [StoreCreatorItem<T, U>, string, T];
-export type ComputedStoreCreator<T> = (arg: T) => Partial<T>;
-export type Bound<T> = [Store<T>, ComputedStoreCreator<T>];
+export type ComputedStoreCreator<T, S> = (arg: T) => S;
 export type SubscribeCallback<T> = (arg: T) => void;
+export type InferredSelector<T, S> = (state: Inferred<T>) => S;
+export type Selector<T, S> = (state: T) => S;
 export type Inferred<S> = S extends { getState: () => infer T } ? T : never;
 export type UseStore<T extends Store<unknown>> = {
     (): Inferred<T>;
-    <S>(selector?: Selector<T, S>): S;
+    <S>(selector?: InferredSelector<T, S>): S;
 } & T;
+
 export type CreateSimple = {
     <T>(creator: StoreCreator<T>): UseStore<Store<T>>;
 };
 export type CreateWithComputed = {
-    <T>(creator: StoreCreator<T>, computed?: ComputedStoreCreator<T>): [UseStore<Store<T>>, UseStore<Store<T>>];
+    <T, S>(creator: StoreCreator<T>, computed?: ComputedStoreCreator<T, S>): [UseStore<Store<T>>, UseStore<Store<S>>];
 };
 
 export interface Store<T> {
