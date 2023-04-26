d554d822df0ee77d3dbf9dd20cf168f59f53a3e0
Andrew <andrew.bubnov75@gmail.com>
2023-04-26 15:21:14 +0200

    Refactor

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import {persist} from "./utils/persist";
 import {
-    ComputedStoreCreator,
+    ComputedStoreCreator, Selector,
     Store,
     StoreCreator,
     UseStore
 } from "./types";
-import {createSimple, createWithComputed} from "./utils/create";
+import { createStore } from "./utils/createStore";
+import { useStore } from "./hooks/useStore";
 
 export function create<T, S extends T = T>(storeCreator: StoreCreator<T, S>): UseStore<Store<T>>;
 
@@ -18,8 +19,9 @@ export function create<T, U = never, S extends T = T>(
     storeCreator: StoreCreator<T, S>,
     computed?: ComputedStoreCreator<T, U>
 ) {
-    if (computed) return createWithComputed(storeCreator, computed);
-    return createSimple(storeCreator);
+    const storeInstance = createStore(storeCreator, computed);
+    const hook = (store: Store<T>, selector?: Selector<T, S>) => useStore(store, selector);
+    return hook.bind(null, storeInstance);
 }
 
 export { persist };
---
+++ utils/createStore.ts
@@ -0,0 +1,37 @@
+import {
+    ComputedStoreCreator,
+    FunctionalParam,
+    ObjectParam,
+    SetStateAction,
+    StoreCreator,
+    SubscribeCallback
+} from "../types";
+import {merge, mergeComputed} from "./merge";
+
+export const createStore = <T, U>(storeCreatorArg: StoreCreator<T>, computed?: ComputedStoreCreator<T, U>) => {
+    const [storeCreator, persistKey, persisted] = Array.isArray(storeCreatorArg) ? storeCreatorArg : [storeCreatorArg];
+
+    let store = {} as T;
+    const subscribers = new Set<SubscribeCallback<T>>();
+
+    const setter = (setStateAction: SetStateAction<T>) => {
+        const isFunction = typeof setStateAction === 'function';
+        const updated = isFunction ? (setStateAction as FunctionalParam<T>)(store) : (setStateAction as ObjectParam<T>);
+        store = merge(store as object, updated);
+        if (computed) store = mergeComputed(store, computed);
+        subscribers.forEach(callback => callback(store));
+    };
+
+    store = computed ? mergeComputed(storeCreator(setter), computed) : storeCreator(setter);
+
+    if (persistKey && persisted) store = merge(store as object, persisted);
+
+    return {
+        getState: () => store,
+        subscribe: (callback: SubscribeCallback<T>) => {
+            subscribers.add(callback);
+            return () => subscribers.delete(callback);
+        },
+        persistKey,
+    };
+};
